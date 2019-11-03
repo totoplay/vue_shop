ce140216f6ef46b5b002a1d675f310011ed54b66
totoplay <1032050177@qq.com>
2019-11-03 14:49:49 +0800

    Enter

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from '../components/login.vue'
+import home from '../components/Home.vue'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: login }]
+    { path: '/login', component: login },
+    { path: '/home', component: home }]
+})
+
+
+router.beforeEach((to,from,next) => {
+  if (to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
