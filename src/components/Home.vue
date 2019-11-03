420e866297aa137d7b1dc61409517f91379dc5e1
totoplay <1032050177@qq.com>
2019-11-03 18:50:38 +0800

    呃呃

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,8 @@
 <template>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< login
-       <el-container class="home-container">
+        
+    <el-container class="home-container">
   <el-header>
+    <el-button type='info' @click='loginout'>退出</el-button>
       <div>
           <img src="../assets/heima.png" alt="">
           <span>电商后台管理系统</span>
@@ -32,59 +31,6 @@
     <el-main>Main</el-main>
   </el-container>
 </el-container>
-=======
-    <div>
-        <el-button type='info' @click='loginout'>退出</el-button>
-    </div>
->>>>>>> master
-=======
-    <div>
-        <el-button type='info' @click='loginout'>退出</el-button>
-    </div>
-=======
-<<<<<<< HEAD
-       <el-container class="home-container">
-  <el-header>
-      <div>
-          <img src="../assets/heima.png" alt="">
-          <span>电商后台管理系统</span>
-      </div>
-      <el-button type='info' @click='loginout'>退出</el-button>
-  </el-header>
-  <el-container>
-    <el-aside width="200px">
-        <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-      </el-submenu>
-    </el-menu>
-    </el-aside>
-    <el-main>Main</el-main>
-  </el-container>
-</el-container>
-=======
-    <div>
-        <el-button type='info' @click='loginout'>退出</el-button>
-    </div>
->>>>>>> master
->>>>>>> login
->>>>>>> local
-=======
-    <div>
-        <el-button type='info' @click='loginout'>退出</el-button>
-    </div>
->>>>>>> master
 </template>
 
 <script>
@@ -99,14 +45,6 @@ export default {
 </script>
 
 <style lang="less" scoped>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< login
-=======
-
-=======
-<<<<<<< HEAD
->>>>>>> local
 .home-container{
     height: 100%;
 }
@@ -121,14 +59,4 @@ export default {
 .el-main{
   background-color:#eaedf1;
 }
-=======
-
->>>>>>> master
-<<<<<<< login
-=======
->>>>>>> login
->>>>>>> local
-=======
-
->>>>>>> master
 </style>
